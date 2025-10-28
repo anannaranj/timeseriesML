53c18fc6e1a7c1ab90c0e68a60bd643174ebec56
CLiPPY <clippy@pi.hole>
2025-10-28 18:22:10 +0300

    undo test

--- main.py
+++ main.py
@@ -9,5 +9,3 @@ df = cleaning()
 # That is what the machine is going to learn
 # as it will take ages to learn it by minutes
 df = asFreq(df, "D")
-
-# just a simple commit because I saw gh registering me not as a github user and just as a git user for some reason
